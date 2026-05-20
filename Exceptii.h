b5e2e8e28b90d9b09d0a870a7994634d35a83909
Lupu Maria Anesia <anisia.lupu.2015@icloud.com>
2026-05-21 00:11:05 +0300

    Completare Exceptii.h: Adauga ExceptieResursa (a 3-a clasa custom) si mesaje imbunatatite

--- Exceptii.h
+++ Exceptii.h
@@ -3,8 +3,9 @@
 
 #include <stdexcept>
 #include <string>
+#include <sstream>
 
-// Exceptii custom pentru clinica veterinara
+// Exceptii custom pentru clinica veterinara - CERINTA 1: Ierarhie proprie minimum 3 clase
 class ExceptieClinica : public std::runtime_error {
 protected:
     int id_; // id-ul asociat erorii, daca exista
@@ -32,5 +33,20 @@ public:
         : ExceptieClinica(msg, id) {}
 };
 
-#endif // PROIECT_POO_EXCEPTII_H
+// CERINTA 1: A 3-a clasa custom pentru erori de resurse (noi)
+class ExceptieResursa : public ExceptieClinica {
+private:
+    int resurseDisponibile;
+public:
+    ExceptieResursa(const std::string &msg, int id = -1, int resurse = 0)
+        : ExceptieClinica(msg, id), resurseDisponibile(resurse) {
+        // Mesaj imbunatatit cu info resurse
+        std::ostringstream oss;
+        oss << msg << " [Resurse disponibile: " << resurse << "]";
+        msg_ = oss.str();
+    }
 
+    int getResurseDisponibile() const noexcept { return resurseDisponibile; }
+};
+
+#endif // PROIECT_POO_EXCEPTII_H
